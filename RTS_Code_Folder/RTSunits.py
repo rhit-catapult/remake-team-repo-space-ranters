d3b204a18a5e29fb6b2d3dbb992f5b39ae5be7aa
MrWill08 <sjwi11iam052108@gmail.com>
2026-06-12 15:52:53 -0400

    v0.4
    v0.4

--- RTS_Code_Folder/RTSunits.py
+++ RTS_Code_Folder/RTSunits.py
@@ -2,12 +2,24 @@ import pygame
 import sys
 import random
 import time
+import math
 
 class Frigate:
-    def __init__(self, screen, x, y, dx, dy):
+    def __init__(self, screen, x, y):
         self.screen = screen
+        self.image = pygame.image.load("RTS_Code_Folder\\Friget_V1.png").convert_alpha()
         self.x, self.y = x, y
-        self.dx, self.dy = dx, dy
+        self.angle = 0
+
+    def draw(self):
+        rotated = pygame.transform.rotate(self.image, self.angle)
+        rotated_rect = rotated.get_rect(center=(self.x, self.y))
+        self.screen.blit(rotated, rotated_rect)
+
+    def move(self, speed, angle):
+        self.angle = angle
+        dx = speed * math.cos((angle + 90) * math.pi / 180)
+        dy = -speed * math.sin((angle + 90) * math.pi / 180)
+        self.x += dx
+        self.y += dy
     
-    def move(self, speed):
-        pass
--- RTS_Code_Folder/RunSpace.py
+++ RTS_Code_Folder/RunSpace.py
@@ -3,6 +3,7 @@ import sys
 import random
 import time
 import RTSunits
+import math
 
 def main():
     pygame.init()
@@ -11,22 +12,42 @@ def main():
     clock = pygame.time.Clock()
     
     # ships: 
+    ship1 = RTSunits.Frigate(screen, 200, 200)
+    speed = 0
+    angle = 0
+    turn_speed = 0
 
     while True:
-        clock.tick(60)  # this sets the framerate of your game
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+        # controls
+        for event in pygame.event.get(): # user interaction events:
+            if event.type == pygame.QUIT: # quit
                 sys.exit()
+            elif event.type == pygame.KEYDOWN: # key down events:
+                if event.key == pygame.K_w: # up
+                    speed = 2
+                elif event.key == pygame.K_s: # down
+                    speed = -2
+                elif event.key == pygame.K_a: # left
+                    turn_speed = 3
+                elif event.key == pygame.K_d: # right
+                    turn_speed = -3
+            if event.type == pygame.KEYUP: # key up events:
+                if event.key in (pygame.K_w, pygame.K_s): # working on deceleration..
+                    if speed > 0:
+                        speed -= 0.5
+                    elif speed < 0:
+                        speed += 0.5
 
-            # TODO: keyboard events here:
+        angle = (angle + turn_speed) % 360
 
-        screen.fill((255, 255, 255))
-
-        # TODO: project code:
-
+        # draw elements
+        screen.fill((0, 0, 0))
+        ship1.draw()
+        ship1.move(speed, angle)
+        
         # update screen:
         clock.tick(60)
         pygame.display.update()
 
-
+    
 main()
